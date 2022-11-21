7466500b1deb5232b2fa47ad62db26763445c1c9
NatiaGotsiridze <101630226+NatiaGo@users.noreply.github.com>
2022-11-21 21:35:56 +0400

    finish

--- script.js
+++ script.js
@@ -30,6 +30,59 @@ fetch("https://reqres.in/api/unknown", {
 
 
 // XML
+let currentPage = 1;
+let totalPages;
+
+function getUsers(page){
+
+let requist = new XMLHttpRequest ();
+
+requist.addEventListener ('load', function(){
+let text = requist.responseText ;
+let jsText = JSON.parse(text)
+
+const fragment = new DocumentFragment();
+
+jsText.data.forEach(item => {
+    let li = document.createElement('li');
+
+    li.innerText = `${item.first_name} ${item.last_name}`;
+    fragment.appendChild(li);
+});
+    document.getElementById('ul-users').innerHTML =" ";  
+    document.getElementById('ul-users').appendChild(fragment);
+    totalPages = jsText.total_pages;
+});
 
 
+requist.addEventListener('error', function(){
+    p.tcontent = 'Server Error';
+
+    document.getElementById('api-users').appendChild(p);
+});
+
+
+requist.open('GET', 'https://reqres.in/api/users?page=' + page);
+requist.send();
+
+}
+
+document.getElementById('prepage').addEventListener('click', function(){
+    if(currentPage==1) {
+    return; 
+}
+    currentPage -= 1;
+    getUsers(currentPage);
+});
+
+document.getElementById('nextPage').addEventListener('click', function(){ 
+    if (currentPage == totalPages){
+        return;
+    }
+    
+    currentPage += 1;
+    getUsers(currentPage);
+});
+
+getUsers(currentPage);
 
